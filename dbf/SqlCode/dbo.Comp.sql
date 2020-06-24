10990817250afff092f8f9495624728ee2e365ae
FrontLook-Admin <frontlook.inc@gmail.com>
2020-06-24 19:57:07 +0530

    update:data designer updated

---
+++ dbf/SqlCode/dbo.Comp.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Comp] (
+    [CompId]         INT           NOT NULL,
+    [Name]           VARCHAR (MAX) NOT NULL,
+    [CompPath]       VARCHAR (MAX) NOT NULL,
+    [BackInterval]   INT           NOT NULL,
+    [IntervalType]   VARCHAR (30)  NOT NULL,
+    [BackUpLocation] VARCHAR (MAX) NOT NULL,
+    [LastBackup]     DATETIME      NOT NULL,
+    [Active]         BIT           NOT NULL, 
+    CONSTRAINT [PK_Comp] PRIMARY KEY ([CompId])
+);
+
---
+++ dbf/SqlCode/dbo.PathData.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[PathData] (
+    [PathId] INT            NOT NULL,
+    [CompId] INT            NOT NULL,
+    [Path]   NVARCHAR (MAX) NOT NULL,
+    CONSTRAINT [PK_PathData] PRIMARY KEY ([PathId]),
+    CONSTRAINT [FK_PathData_Comp] FOREIGN KEY ([CompId]) REFERENCES [Comp]([CompId]) 
+);
+
